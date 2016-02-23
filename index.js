14126367673c5e365f9870fc31a2ebd09c61641e
Matt <mattc65@gmail.com>
2016-02-23 19:41:39 +0000

    Update API functionality, to allow regenerating auth-codes and other QOL features

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ app.all('/register', function(req, res) {
     redis.get(uquery).then(function (result) {
         if (result !== "" && result !== undefined && result !== null) {
             res.send({"registered": 0,
-                      "error": 1});
+                     "error": 1});
         } else {
             bcrypt.hash(tmp_password, null, null, function (err, hash) {
                 user_object["password"] = hash;
                 user_object["auth-key"] = bcrypt.hashSync(Date.now().toString() + tmp_username);
                 redis.set(uquery, JSON.stringify(user_object));
                 res.send({"registered": 1,
-                          "auth-key": user_object["auth-key"],
-                          "error": 0});
+                         "auth-key": user_object["auth-key"],
+                         "error": 0});
             });
         }
     });
@@ -39,36 +39,52 @@ app.all('/register', function(req, res) {
 app.all('/login', function(req, res) {
     var username = req.body.user || req.query.user;
     var password = req.body.password || req.query.password;
+    var auth_key = req.body.auth_key || req.query.auth_key;
     var uquery   = 'user:' + username;
 
     redis.get(uquery).then(function (result) {
         if (result !== "" && result !== undefined && result !== null) {
             var user_object = JSON.parse(result);
-            bcrypt.compare(password, user_object["password"], function (err, matched) {
-                if (matched === true) {
-                    if (undefined === user_object["auth-key"]) {
-                        var timestamp_user = Date.now().toString() + username;
-                        user_object["auth-key"] = bcrypt.hashSync(timestamp_user);
-                        redis.set(uquery, JSON.stringify(user_object));
-                        res.send({"logged_in": 1,
-                                  "auth-key": user_object["auth-key"],
-                                  "error": 0});
-                    } else {
-                        res.send({"logged_in": 1,
-                                  "auth-key": user_object["auth-key"],
-                                  "error": 0});
-                    }
-                    return;
+            if (auth_key !== "" && auth_key !== undefined && auth_key !== null) {
+                if (auth_key === user_object["auth-key"]) {
+                    var timestamp_user = Date.now().toString() + username;
+                    user_object["auth-key"] = bcrypt.hashSync(timestamp_user);
+                    redis.set(uquery, JSON.stringify(user_object));
+                    res.send({"logged_in": 1,
+                             "auth-key": user_object["auth-key"],
+                             "error": 0});
                 } else {
                     res.send({"logged_in": 0,
-                              "error": 2});
-                    return;
+                             "error": 3});
                 }
-            });
+            } else {
+                bcrypt.compare(password, user_object["password"], function (err, matched) {
+                    if (matched === true) {
+                        if (undefined === user_object["auth-key"]) {
+                            var timestamp_user = Date.now().toString() + username;
+                            user_object["auth-key"] = bcrypt.hashSync(timestamp_user);
+                            redis.set(uquery, JSON.stringify(user_object));
+                            res.send({"logged_in": 1,
+                                     "auth-key": user_object["auth-key"],
+                                     "error": 0});
+                        } else {
+                            res.send({"logged_in": 1,
+                                     "auth-key": user_object["auth-key"],
+                                     "error": 0});
+                        }
+                        return;
+                    } else {
+                        res.send({"logged_in": 0,
+                                 "error": 2});
+                                 return;
+                    }
+                });
+
+            }
         } else {
             res.send({"logged_in": 0,
-                      "error": 1});
-            return;
+                     "error": 1});
+                     return;
         }
     });
 });
@@ -78,6 +94,6 @@ app.listen(3000, function () {
 });
 
 process.on('SIGINT', function() {
-  console.log( "\nRecieved Ctrl-C, shutting down." );
-  process.exit(0);
+    console.log( "\nRecieved Ctrl-C, shutting down." );
+    process.exit(0);
 })
