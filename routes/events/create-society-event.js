7db89841cb7523ff606fae0d090adcb5256b2d0c
Matt <mattc65@gmail.com>
2016-04-14 12:17:03 +0100

    fuck

--- routes/events/create-society-event.js
+++ routes/events/create-society-event.js
@@ -7,15 +7,22 @@ module.exports = {
 }
 
 var perform = function (req, res) {
-  var soc_name = decodeURIComponent(req.body.society || req.query.society);
-  var event_name = decodeURIComponent(req.body.name || req.query.name);
-  var event_loc = decodeURIComponent(req.body.location || req.query.location);
-  var event_start = decodeURIComponent(req.body.start || req.query.start);
-  var event_end = decodeURIComponent(req.body.end || req.query.end);
-  var event_details = decodeURIComponent(req.body.details || req.query.details);
-  var auth_key = decodeURIComponent(req.body.auth || req.query.auth);
+  var soc_name = req.body.society || req.query.society;
+  var event_name = req.body.name || req.query.name;
+  var event_loc = req.body.location || req.query.location;
+  var event_start = req.body.start || req.query.start;
+  var event_end = req.body.end || req.query.end;
+  var event_details = req.body.details || req.query.details;
+  var auth_key = req.body.auth || req.query.auth;
 
   if (soc_name && event_name && event_loc && event_start && event_end && event_details && auth_key) {
+    soc_name = decodeURIComponent(soc_name);
+    event_name = decodeURIComponent(event_name);
+    event_loc = decodeURIComponent(event_loc);
+    event_start = decodeURIComponent(event_start);
+    event_end = decodeURIComponent(event_end);
+    event_details = decodeURIComponent(event_details);
+    auth_key = decodeURIComponent(auth_key);
     var event = {
       name: event_name,
       location: event_loc,
