aaf959a0fb516cf81ca47db2632bad3acc755506
Matt <mattc65@gmail.com>
2016-04-14 19:46:41 +0100

    Add proper logging and production environment

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ var https = require('https');
 var app = express();
 var route_manager = require("./utils/route-manager.js");
 var bodyParser = require('body-parser');
+var FileStreamRotator = require('file-stream-rotator');
+var morgan = require('morgan');
 var fs = require('fs');
-var prkey = fs.readFileSync('server.key');
-var certi = fs.readFileSync('server.crt');
+var logDirectory = 'log'
+var production = process.argv[2];
+
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory)
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  date_format: 'YYYYMMDD',
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+})
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
+app.use(morgan('combined'));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,8 +36,20 @@ app.use('/', route_manager);
 app.use('/source', require('magic-window')('/source', { ignore: ['config'] }))
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Now accepting connections on port 3000.');
 });
+
+if (production === "-p") {
+  var prkey = fs.readFileSync('key.pem');
+  var certi = fs.readFileSync('cert.pem');
+
+  https.createServer({
+    key: prkey,
+    cert: certi
+  }, app).listen(443, function() {
+    console.log("Now accepting HTTPS connections on port 443.");
+  });
+}
 
 process.on('SIGINT', function() {
   console.log( "\nRecieved Ctrl-C, shutting down." );
