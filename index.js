84914be4c2e1a24ce5750387ee828c10f185d928
Matt <mattc65@gmail.com>
2016-04-17 03:38:25 +0100

    Fix travis builds

--- index.js
+++ index.js
@@ -59,8 +59,10 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/', route_manager);
 app.use('/source', require('magic-window')('/source', { ignore: ['config', 'redis', 'cert.pem', 'key.pem', 'dump.rdb', 'logpasswd'] }))
-app.use('/log', auth, express.static('log'));
-app.use('/log', auth, serveIndex('log', {'icons': true}));
+if (production === "-p") {
+  app.use('/log', auth, express.static('log'));
+  app.use('/log', auth, serveIndex('log', {'icons': true}));
+}
 
 app.listen(3000, function () {
   console.log('Now accepting connections on port 3000.');
