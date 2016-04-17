20b411d4dd23d3aec527255906d0f4aef6343d40
Matt <mattc65@gmail.com>
2016-04-17 16:15:41 +0100

    Add unit tests for the /user/auth/ route

--- spec/user-ops-spec.js
+++ spec/user-ops-spec.js
@@ -3,20 +3,33 @@ var base_url = "http://localhost:3000";
 
 describe("User Operations", function () {
 
-  var register_user = {
-    url: base_url + "/user/register",
-    method: "POST",
-    json: {
-      user: "foo123",
-      password: "foofoo"
-    }
+  var register_user = function (username) {
+    return {
+      url: base_url + "/user/register/",
+      method: "POST",
+      json: {
+        user: username,
+        password: "foofoo"
+      }
+    };
+  };
+
+  var login_user = function (username, pass) {
+    return {
+      url: base_url + "/user/auth/",
+      method: "POST",
+      json: {
+        user: username,
+        password: pass
+      }
+    };
   };
 
   var user_ops_auth_key = "";
 
   describe("POST /user/register/", function () {
     it("can register user", function (done) {
-      request(register_user, function (error, response, body) {
+      request(register_user("foo123"), function (error, response, body) {
         expect(response.statusCode).toBe(200);
         expect(body.registered).toBe(1);
         if (body["auth-key"]) {
@@ -27,8 +40,8 @@ describe("User Operations", function () {
       });
     });
 
-    it ("cannot register the same user twice", function (done) {
-      request(register_user, function (error, response, body) {
+    it("cannot register the same user twice", function (done) {
+      request(register_user("foo123"), function (error, response, body) {
         expect(response.statusCode).toBe(200);
         expect(body.registered).toBe(0);
         expect(body["auth-key"]).toBe(undefined);
@@ -37,7 +50,7 @@ describe("User Operations", function () {
       });
     });
 
-    it ("cannot accept malformed requests", function (done) {
+    it("cannot accept malformed requests", function (done) {
       request({
         url: base_url + "/user/register/",
         method: "POST",
@@ -52,7 +65,56 @@ describe("User Operations", function () {
         expect(body.error).toBe(2);
         done();
       });
-    })
-  });
+    });
+  }); //end POST /user/register/
+
+  describe("POST /user/auth/", function () {
+    it("can login as an existing user", function (done) {
+      request(login_user("foo123", "foofoo"), function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        expect(body.logged_in).toBe(1);
+        expect(body["auth-key"].length).toBe(60);
+        expect(body.error).toBe(0);
+        done();
+      });
+    });
+
+    it("cannot login with an incorrect password", function (done) {
+      request(login_user("foo123", "foofo"), function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        expect(body.logged_in).toBe(0);
+        expect(body["auth-key"]).toBe(undefined);
+        expect(body.error).toBe(1);
+        done();
+      });
+    });
+
+    it("cannot login with an incorrect username", function (done) {
+      request(login_user("foo1233", "foofoo"), function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        expect(body.logged_in).toBe(0);
+        expect(body["auth-key"]).toBe(undefined);
+        expect(body.error).toBe(1);
+        done();
+      });
+    });
+
+    it("cannot accept malformed requests", function (done) {
+      request({
+        url: base_url + "/user/auth/",
+        method: "POST",
+        json: {
+          usr: "foo123",
+          password: "foofoo"
+        }
+      }, function (error, response, body) {
+        expect(response.statusCode).toBe(200);
+        expect(body.logged_in).toBe(0);
+        expect(body["auth-key"]).toBe(undefined);
+        expect(body.error).toBe(2);
+        done();
+      });
+    });
+  }); //end POST /user/auth/
 
 });
